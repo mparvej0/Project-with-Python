4937842586bcc332019de475711ad9c140dc8c7d
MD PARVEJ <mparvejind@gmail.com>
2025-01-06 21:12:05 +0530

    upd

---
+++ image_face_detection.py
@@ -0,0 +1,18 @@
+import cv2
+
+#Load pre-trained face classifier
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+#Read the image (for image file)
+image = cv2.imread("Pic.jpg")
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+cv2.imshow('Face Detection', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
